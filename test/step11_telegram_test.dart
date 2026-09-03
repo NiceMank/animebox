16679cfa5bdb6d56e62bb185981f236872431a4e
NiceMank <nicemank@users.noreply.github.com>
2026-09-03 05:14:52 +0000

    Analyse : import TelegramSource (step11)

--- test/step11_telegram_test.dart
+++ test/step11_telegram_test.dart
@@ -4,6 +4,7 @@ import 'package:animebox/features/anime/data/repositories/local_anime_repository
 import 'package:animebox/features/local/data/local_database.dart';
 import 'package:animebox/features/telegram/data/gateway/telegram_gateway.dart';
 import 'package:animebox/features/telegram/data/models/api_exception.dart';
+import 'package:animebox/features/telegram/data/models/telegram_source.dart';
 import 'package:animebox/features/telegram/data/services/local_telegram_service.dart';
 import 'package:animebox/features/telegram/data/services/telegram_service.dart';
 import 'package:animebox/features/telegram/data/services/telegram_session_manager.dart';
